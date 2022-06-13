5a6f29eedb01951e452a578f9e44850eda6069dd
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-06-13 12:33:29 +0300

    restframework response and decorators

--- project/api/views.py
+++ project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from project.models import Post
 from .serializers import  PostSerializer
-#
+
 
 
 @api_view(['GET'])
